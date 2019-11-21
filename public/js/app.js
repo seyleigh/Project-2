ee075b193357eba5c79af24e01a6b342f43e2f54
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-20 20:45:56 -0500

    added particles and changed html routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-// console.log('Hi');
+console.log('Hi');
 
-// particlesJS.load('particles-js', '../particles.json', function () {
-//     console.log('callback - particles-js config loaded');
-//   });
+particlesJS.load('particles-js', '/public/js/particles.json', function () {
+    console.log('callback - particles-js config loaded');
+  });
