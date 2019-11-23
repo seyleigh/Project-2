e6eeec3c014c024517cf355453e079f83acea6a5
Jake Campbell <jakecmail@gmail.com>
2019-11-23 12:01:30 -0500

    Added starter Hauntings, UFOs and 404 pages and updated HTML routes to load them

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,33 @@ var db = require("../models");
 var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
-  
+  // root route loads index.html page (to prompt login)
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
-  // Load example page and pass in an example by id
-  app.get("/home", function (req, res) {
+  // home route loads home page
+  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
-    // db.Example.findOne({ where: { id: req.params.id } }).then(function(
-    //   dbExample
-    // ) {
-    //   res.render("example", {
-    //     example: dbExample
-    //   });
-    // });
+  });
+
+  // ufos route loads ufos.html page
+  app.get("/ufos", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ufos.html"));
+  });
+
+  // hauntings route loads hauntings.html page
+  app.get("/hauntings", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/hauntings.html"));
+  });
+
+  // maps route loads search-ufo-haunts.html page
+  app.get("/maps", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search-ufo-haunts.html"));
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
