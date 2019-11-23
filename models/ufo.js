cf129ab0f7e8562412120c24f87c1ef86baa3dc5
Matt Wilson <matt@firstflightagency.com>
2019-11-23 11:21:38 -0500

    commented models out again

--- models/ufo.js
+++ models/ufo.js
@@ -1,12 +1,12 @@
-module.exports = function(sequelize, DataTypes) {
-  var nuforc_reports = sequelize.define("nuforc_reports", {
-    city: DataTypes.STRING,
-    state: DataTypes.STRING,
-    shape: DataTypes.STRING,
-    duration: DataTypes.STRING,
-    text: DataTypes.TEXT,
-    city_latitude: DataTypes.DECIMAL(8, 8),
-    city_longitude: DataTypes.DECIMAL(8, 8)
-  });
-  return nuforc_reports;
-};
+// module.exports = function(sequelize, DataTypes) {
+//   var nuforc_reports = sequelize.define("nuforc_reports", {
+//     city: DataTypes.STRING,
+//     state: DataTypes.STRING,
+//     shape: DataTypes.STRING,
+//     duration: DataTypes.STRING,
+//     text: DataTypes.TEXT,
+//     city_latitude: DataTypes.DECIMAL(8, 8),
+//     city_longitude: DataTypes.DECIMAL(8, 8)
+//   });
+//   return nuforc_reports;
+// };
