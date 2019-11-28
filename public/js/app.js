c682ada25c8d6d3d6c5496b9e4c1b8d96b18fc66
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-27 21:28:21 -0500

    added more css, mostly adjusted padding and added a logout

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
-console.log('Hi');
+// console.log('Hi');
 
 particlesJS.load('particles-js', './js/particles.json', function () {
-  console.log('callback - particles-js config loaded');
+  // console.log('callback - particles-js config loaded');
 });
 // particlesJS.load('particles-js2', './js/particles2.json', function () {
 //   console.log('callback - particles-js2 config loaded');
