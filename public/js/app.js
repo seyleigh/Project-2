47d1eaf2bad1082e39e6cf04b926b6a99b54bee9
Ian Applekamp <ieapplekamp@gmail.com>
2019-12-01 16:53:42 -0500

    Added a second particles effect to the home page

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,3 @@ console.log('Hi');
 particlesJS.load('particles-js', './js/particles.json', function () {
   console.log('callback - particles-js config loaded');
 });
-// particlesJS.load('particles-js2', './js/particles2.json', function () {
-//   console.log('callback - particles-js2 config loaded');
-// });
---
+++ public/js/app2.js
@@ -0,0 +1,5 @@
+console.log("hi");
+
+particlesJS.load('particles-js2', './js/particles2.json', function () {
+    console.log('callback - particles-js2 config loaded');
+  });
