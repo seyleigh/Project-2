76276c61802b3fab938953fb721b246ab3281626
Ian Applekamp <ieapplekamp@gmail.com>
2019-12-01 21:56:02 -0500

    This should fix the deployed version maybe

--- models/schema.sql
+++ models/schema.sql
@@ -1,4 +1,8 @@
 DROP DATABASE IF EXISTS cryptids_db;
 CREATE DATABASE cryptids_db;
 
+USE cryptids_db;
+ALTER TABLE nuforc_reports MODIFY city_latitude BIGINT;
+ALTER TABLE nuforc_reports MODIFY city_longitude BIGINT;
+
 
